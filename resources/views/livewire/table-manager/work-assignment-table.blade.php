522285b35278aa0b7feb3ad2c1f5dfcf8b04ce59
Marco <ilbullo@gmail.com>
2025-11-20 20:27:42 +0100

    fixed colours on work assignment view

--- resources/views/livewire/table-manager/work-assignment-table.blade.php
+++ resources/views/livewire/table-manager/work-assignment-table.blade.php
@@ -39,14 +39,15 @@
                                     @php
                                         $index = $slot + 1;
                                     @endphp
-                                    <td class="p-1 text-center text-xs border cursor-pointer"
+                                    <td 
                                         @class([
+                                            'p-1 text-center text-xs border cursor-pointer',
                                             'bg-gray-50 hover:bg-gray-100' => !isset($license['worksMap'][$slot]),
                                             'bg-blue-100' => isset($license['worksMap'][$slot]) && $license['worksMap'][$slot]['value'] === 'A',
                                             'bg-green-100' => isset($license['worksMap'][$slot]) && $license['worksMap'][$slot]['value'] === 'X',
                                             'bg-red-100' => isset($license['worksMap'][$slot]) && $license['worksMap'][$slot]['value'] === 'P',
                                             'bg-yellow-100' => isset($license['worksMap'][$slot]) && $license['worksMap'][$slot]['value'] === 'N',
-                                        ])
+                                        ]) 
                                         wire:click="@if (isset($license['worksMap'][$slot])) openConfirmRemove({{ $license['id'] }}, {{ $index }}) @else assignWork({{ $license['id'] }}, {{ $index }}) @endif">
                                         @if (isset($license['worksMap'][$slot]))
                                             <span class="text-gray-900 font-medium">

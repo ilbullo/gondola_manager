8d8a038c7d40738810c565e3fd9af01a8a74870c
Marco <ilbullo@gmail.com>
2026-01-14 09:48:09 +0100

    fix table-manager layout via vite

--- resources/views/layouts/table-manager.blade.php
+++ resources/views/layouts/table-manager.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>{{ env('APP_NAME',config('app_settings.system_title')) }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
  <!--   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>-->
 <style>
     [x-cloak] { display: none !important; }
